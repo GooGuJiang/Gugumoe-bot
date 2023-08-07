e4b025f3169c433c0025200d7f90ad8051ce6021
咕谷酱 <gu@gmoe.cc>
2023-08-07 10:53:39 +0800

    ✏ 修改读取配置设定

--- gugumoe_bot/settings.py
+++ gugumoe_bot/settings.py
@@ -1,9 +1,6 @@
-import sys
 from pathlib import Path
 from tempfile import gettempdir
 
-from loguru import logger
-from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 TEMP_DIR = Path(gettempdir())
@@ -16,11 +13,13 @@ class Settings(BaseSettings):
 
     log_level: str = "INFO"
 
-    # Add Field for required fields
-    token: str = Field(None, description="Bot token")
+    # Bot token
+    token: str = ""
     proxy: str = ""
-    username: str = Field(None, description="Username")
-    mongodb_url: str = Field(None, description="MongoDB URL")
+    username: str = ""
+
+    # MongoDB settings
+    mongodb_url: str = ""
     mongodb_db_name: str = "gugumoe_bot"
     mongodb_collection_jrrp: str = "jrrp"
 
@@ -29,15 +28,6 @@ class Settings(BaseSettings):
         env_prefix="GUGUMOE_BOT_",
         env_file_encoding="utf-8",
     )
-
-    def __post_init__(self):
-        # List of required attributes
-        required_attributes = ['token', 'username', 'mongodb_url']
-
-        for attribute in required_attributes:
-            if getattr(self, attribute) in ("", None):
-                logger.error(f"Missing required attribute: {attribute}")
-                sys.exit(1)
 
 
 settings = Settings()
